dcac0d6bc03cb03ab62fe5418426e05d31e52d54
ryochack <ryochack@gmail.com>
2015-11-09 02:57:08 +0900

    Fix a problem that runs full-build is run when 'make all' after 'make build-test'
    Problem is build trigger includes _build/obj directory.

--- rules.mk
+++ rules.mk
@@ -1,20 +1,14 @@
-$(TARGET_LIB): $(OBJECTS) $(BUILD_LIB_DIR)
+$(TARGET_LIB): $(OBJECTS)
+	@[ -d $(BUILD_LIB_DIR) ] || mkdir -p $(BUILD_LIB_DIR)
 	$(AR) $(ARFLAGS) $@ $(OBJECTS)
 
-$(TARGET_TEST): $(OBJECTS) $(BUILD_TEST_DIR)
+$(TARGET_TEST): $(OBJECTS)
+	@[ -d $(BUILD_TEST_DIR) ] || mkdir -p $(BUILD_TEST_DIR)
 	$(CC) $(LDFLGAS) $(LIBDIRS) $(LIBS) -o $@ $(OBJECTS)
 
-$(BUILD_OBJ_DIR)/%.o:%.c $(BUILD_OBJ_DIR)
+$(BUILD_OBJ_DIR)/%.o:%.c
+	@[ -d $(BUILD_OBJ_DIR) ] || mkdir -p $(BUILD_OBJ_DIR)
 	$(CC) $(CFLAGS) $(INCLUDES) -o $@ -c $<
-
-$(BUILD_OBJ_DIR):
-	mkdir -p $@
-
-$(BUILD_LIB_DIR):
-	mkdir -p $@
-
-$(BUILD_TEST_DIR):
-	mkdir -p $@
 
 -include $(DEPENDS)
 
