71c32d1d3d7e9413f9621a8473b0e86cd5338aed
ryochack <ryochack@gmail.com>
2015-11-12 01:35:19 +0900

    Replace TEST_ASSERT_EQUAL_UINT8() with TEST_ASSERT_EQUAL_UINT8_ARRAY() for comparing array

--- test/container/fixed_ringbuffer_test.c
+++ test/container/fixed_ringbuffer_test.c
@@ -29,6 +29,7 @@ TEST(fixed_ringbuffer, create)
 TEST(fixed_ringbuffer, destroy)
 {
 	s_handle = fixedringbuf_create(s_pool, s_blocksize, s_blockcount);
+	TEST_ASSERT_NOT_NULL( s_handle );
 	TEST_ASSERT_UNLESS( fixedringbuf_destroy(s_handle) );
 	TEST_ASSERT_NULL( fixedringbuf_get(s_handle) );
 }
@@ -42,20 +43,18 @@ TEST(fixed_ringbuffer, write_read)
 		  0x18, 0x19, 0x1A, 0x1B, 0x1C, 0x1D, 0x1E, 0x1F }
 	};
 	uint8_t rxblk[BLOCK_SIZE] = { 0 };
-	int32_t i;
+
 	s_handle = fixedringbuf_create(s_pool, s_blocksize, s_blockcount);
+	TEST_ASSERT_NOT_NULL( s_handle );
+
 	TEST_ASSERT_UNLESS( fixedringbuf_write(s_handle, txblk[0]) );
 	TEST_ASSERT_UNLESS( fixedringbuf_write(s_handle, txblk[1]) );
 
 	TEST_ASSERT_UNLESS( fixedringbuf_read(s_handle, rxblk) );
-	for (i=0; i<(int32_t)sizeof(rxblk); i++) {
-		TEST_ASSERT_EQUAL_UINT8( i, rxblk[i] );
-	}
+	TEST_ASSERT_EQUAL_UINT8_ARRAY( txblk[0], rxblk, BLOCK_SIZE );
 
 	TEST_ASSERT_UNLESS( fixedringbuf_read(s_handle, rxblk) );
-	for (i=0; i<(int32_t)sizeof(rxblk); i++) {
-		TEST_ASSERT_EQUAL_UINT8( (0x10 + i), rxblk[i] );
-	}
+	TEST_ASSERT_EQUAL_UINT8_ARRAY( txblk[1], rxblk, BLOCK_SIZE );
 }
 
 TEST(fixed_ringbuffer, get)
@@ -67,20 +66,18 @@ TEST(fixed_ringbuffer, get)
 		  0x18, 0x19, 0x1A, 0x1B, 0x1C, 0x1D, 0x1E, 0x1F }
 	};
 	uint8_t *rxp;
-	int32_t i;
+
 	s_handle = fixedringbuf_create(s_pool, s_blocksize, s_blockcount);
+	TEST_ASSERT_NOT_NULL( s_handle );
+
 	TEST_ASSERT_UNLESS( fixedringbuf_write(s_handle, txblk[0]) );
 	TEST_ASSERT_UNLESS( fixedringbuf_write(s_handle, txblk[1]) );
 
 	TEST_ASSERT_NOT_NULL( (rxp = fixedringbuf_get(s_handle)) );
-	for (i=0; i<(int32_t)sizeof(txblk[0]); i++) {
-		TEST_ASSERT_EQUAL_UINT8( i, rxp[i] );
-	}
+	TEST_ASSERT_EQUAL_UINT8_ARRAY( txblk[0], rxp, BLOCK_SIZE );
 
 	TEST_ASSERT_NOT_NULL( (rxp = fixedringbuf_get(s_handle)) );
-	for (i=0; i<(int32_t)sizeof(txblk[0]); i++) {
-		TEST_ASSERT_EQUAL_UINT8( (0x10 + i), rxp[i] );
-	}
+	TEST_ASSERT_EQUAL_UINT8_ARRAY( txblk[1], rxp, BLOCK_SIZE );
 }
 
 TEST(fixed_ringbuffer, size)
@@ -91,7 +88,9 @@ TEST(fixed_ringbuffer, size)
 	};
 	uint8_t *rxp;
 	uint32_t using = 0;
+
 	s_handle = fixedringbuf_create(s_pool, s_blocksize, s_blockcount);
+	TEST_ASSERT_NOT_NULL( s_handle );
 
 	TEST_ASSERT_EQUAL_UINT32( s_blockcount, fixedringbuf_capacity(s_handle) );
 	TEST_ASSERT_EQUAL_UINT32( s_blockcount, fixedringbuf_available(s_handle) );
@@ -127,7 +126,9 @@ TEST(fixed_ringbuffer, purge)
 		0x08, 0x09, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F
 	};
 	uint32_t using = 0;
+
 	s_handle = fixedringbuf_create(s_pool, s_blocksize, s_blockcount);
+	TEST_ASSERT_NOT_NULL( s_handle );
 
 	TEST_ASSERT_EQUAL_UINT32( s_blockcount, fixedringbuf_available(s_handle) );
 	TEST_ASSERT_EQUAL_UINT32( 0, fixedringbuf_used(s_handle) );
@@ -160,8 +161,9 @@ TEST(fixed_ringbuffer, write_overflow)
 		  0x48, 0x49, 0x4A, 0x4B, 0x4C, 0x4D, 0x4E, 0x4F }
 	};
 	uint8_t *rxp;
-	int32_t i;
+
 	s_handle = fixedringbuf_create(s_pool, s_blocksize, s_blockcount);
+	TEST_ASSERT_NOT_NULL( s_handle );
 
 	TEST_ASSERT_UNLESS( fixedringbuf_write(s_handle, txblk[0]) );
 	TEST_ASSERT_UNLESS( fixedringbuf_write(s_handle, txblk[1]) );
@@ -176,21 +178,16 @@ TEST(fixed_ringbuffer, write_overflow)
 	TEST_ASSERT_EQUAL_UINT32( s_blockcount, fixedringbuf_used(s_handle) );
 
 	TEST_ASSERT_NOT_NULL( (rxp = fixedringbuf_get(s_handle)) );
-	for (i=0; i<(int32_t)sizeof(txblk[0]); i++) {
-		TEST_ASSERT_EQUAL_UINT8( (0x10 + i), rxp[i] );
-	}
+	TEST_ASSERT_EQUAL_UINT8_ARRAY( txblk[1], rxp, BLOCK_SIZE );
+
 	TEST_ASSERT_NOT_NULL( (rxp = fixedringbuf_get(s_handle)) );
-	for (i=0; i<(int32_t)sizeof(txblk[0]); i++) {
-		TEST_ASSERT_EQUAL_UINT8( (0x20 + i), rxp[i] );
-	}
+	TEST_ASSERT_EQUAL_UINT8_ARRAY( txblk[2], rxp, BLOCK_SIZE );
+
 	TEST_ASSERT_NOT_NULL( (rxp = fixedringbuf_get(s_handle)) );
-	for (i=0; i<(int32_t)sizeof(txblk[0]); i++) {
-		TEST_ASSERT_EQUAL_UINT8( (0x30 + i), rxp[i] );
-	}
+	TEST_ASSERT_EQUAL_UINT8_ARRAY( txblk[3], rxp, BLOCK_SIZE );
+
 	TEST_ASSERT_NOT_NULL( (rxp = fixedringbuf_get(s_handle)) );
-	for (i=0; i<(int32_t)sizeof(txblk[0]); i++) {
-		TEST_ASSERT_EQUAL_UINT8( (0x40 + i), rxp[i] );
-	}
+	TEST_ASSERT_EQUAL_UINT8_ARRAY( txblk[4], rxp, BLOCK_SIZE );
 
 	TEST_ASSERT_NULL( (rxp = fixedringbuf_get(s_handle)) );
 }
