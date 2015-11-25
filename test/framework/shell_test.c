7903c7844ab93c7cc9a3167222dda5baf95d3f7b
ryochack <ryochack@gmail.com>
2015-11-25 23:39:45 +0900

    Add framework/shell_create()/destroy() tests

--- test/framework/shell_test.c
+++ test/framework/shell_test.c
@@ -3,20 +3,43 @@
 
 TEST_GROUP(shell);
 
+static handle_t s_handle = NULL;
+#define POOL_SIZE 256
+static uint8_t s_pool[POOL_SIZE];
+
+static error_t stub_putc(char c) {
+	return 0;
+}
+
+static error_t stub_getc(char c) {
+	return 0;
+}
+
+static error_t stub_exec(char *line) {
+	return 0;
+}
+
 TEST_SETUP(shell)
 {
 }
 
 TEST_TEAR_DOWN(shell)
 {
+	shell_destroy(s_handle);
 }
 
 TEST(shell, create)
 {
+	s_handle = shell_create(s_pool, POOL_SIZE, stub_putc, stub_getc, stub_exec);
+	TEST_ASSERT_NOT_NULL( s_handle );
 }
 
 TEST(shell, destroy)
 {
+	s_handle = shell_create(s_pool, POOL_SIZE, stub_putc, stub_getc, stub_exec);
+	TEST_ASSERT_NOT_NULL( s_handle );
+	TEST_ASSERT_UNLESS( shell_destroy(s_handle) );
+	TEST_ASSERT( shell_start(s_handle) );
 }
 
 TEST(shell, start)
